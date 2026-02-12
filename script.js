8bf74053a9945699e81a3b06800eae7d033931f4
ccodemystique <156036556+ccodemystique@users.noreply.github.com>
2026-02-12 19:49:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 const box = 15;
 let snake = [{ x: 150, y: 150 }];
 let direction = "right";
+let gameStarted = false;
 
 let apple = randomApple();
 let eatCount = 0;
@@ -16,15 +17,27 @@ function randomApple() {
 }
 
 function changeDirection(dir) {
-  direction = dir;
+  if (
+    (dir === "left" && direction !== "right") ||
+    (dir === "right" && direction !== "left") ||
+    (dir === "up" && direction !== "down") ||
+    (dir === "down" && direction !== "up")
+  ) {
+    direction = dir;
+    gameStarted = true;
+  }
 }
 
 function draw() {
+  if (!gameStarted) return;
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Apple
   ctx.fillStyle = "red";
   ctx.fillRect(apple.x, apple.y, box, box);
 
+  // Snake
   ctx.fillStyle = "lime";
   snake.forEach(p => ctx.fillRect(p.x, p.y, box, box));
 
@@ -35,12 +48,25 @@ function draw() {
   if (direction === "up") head.y -= box;
   if (direction === "down") head.y += box;
 
+  // Duvara çarpma
+  if (
+    head.x < 0 ||
+    head.y < 0 ||
+    head.x >= canvas.width ||
+    head.y >= canvas.height
+  ) {
+    alert("Oyun Bitti 💀");
+    location.reload();
+    return;
+  }
+
+  // Elma yeme
   if (head.x === apple.x && head.y === apple.y) {
     eatCount++;
     apple = randomApple();
 
     if (eatCount === 2) {
-      window.location.href = "image.jpg";
+      window.location.href = "image.jpg"; // senin foto
       return;
     }
   } else {
